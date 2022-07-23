0e1ca14d4cc782a2bdaaf608aa8d11dfd9253d4f
roddie digital <roddie-digital@users.noreply.github.com>
2022-07-23 17:09:13 +0100

    Just made it whole lot less bad

--- crlgen.sh
+++ crlgen.sh
@@ -1,30 +1,33 @@
 #!/bin/sh
-# 2019-04-01 https://roddie.digital
+# 2021-07-23 https://roddie.digital
 # Check and regenerate OpenVPN CRL
-
-# move into the right dir
-cd /etc/openvpn/easy-rsa
+# Run as root
 
 # check location of crl
-sudo cat /etc/openvpn/server.conf | grep "crl-verify"
+grep "crl-verify" /etc/openvpn/server.conf
 
 read -p "Press Enter to continue ..."
 
 # check date of current crl
 echo "Checking /etc/openvpn/crl.pem ..."
 echo ""
-sudo openssl crl -in /etc/openvpn/crl.pem -text | grep Next
+openssl crl -in /etc/openvpn/crl.pem -text | grep Next
 
 read -p "Press Enter to continue ..."
 
 # generate the new crl
-sudo ./easyrsa gen-crl
+/etc/openvpn/easy-rsa/easyrsa gen-crl
 
 # copy it to the right location
-sudo cp -v /etc/openvpn/easy-rsa/pki/crl.pem ../crl.pem
+cp -v /etc/openvpn/easy-rsa/pki/crl.pem /etc/openvpn/crl.pem
 
 # restart openvpn
-sudo systemctl restart openvpn
+systemctl restart openvpn
 
 # check openvpn
 systemctl status openvpn
+
+# check the new expiry date
+echo "New expiry date:"
+echo ""
+grep "crl-verify" /etc/openvpn/server.conf
